77d43664c9471c76c8d23755e7e4d1e5deefa94f
SHINE-six <desmondfoo0610@gmail.com>
2025-01-23 20:17:57 +0800

    typo

--- modules/projecttwin/projecttwin.nix
+++ modules/projecttwin/projecttwin.nix
@@ -3,6 +3,6 @@
 {
   networking.extraHosts = ''
     127.0.0.1 redpanda-console.projecttwin.com
-    127.0.0.1 kubenetes-dashboard.projecttwin.com
+    127.0.0.1 kubernetes-dashboard.projecttwin.com
   '';
 }
