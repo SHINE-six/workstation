62499f283515f6cfa7f4b600a8c01ffe74a495e5
SHINE-six <desmondfoo0610@gmail.com>
2025-02-17 13:43:50 +0800

    add zsh shell alias for Redpanda kubectl command, rpk, is redpanda cli

--- modules/projecttwin/projecttwin.nix
+++ modules/projecttwin/projecttwin.nix
@@ -6,4 +6,10 @@
     127.0.0.1 api.projecttwin.com
     # 127.0.0.1 kubernetes-dashboard.projecttwin.com
   '';
+
+  programs.zsh = {
+    shellAliases = {
+      rpk = "kubectl --namespace redpanda exec -i -t redpanda-0 -c redpanda -- rpk";
+    };
+  };
 }
